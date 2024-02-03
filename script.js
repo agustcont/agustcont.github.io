9e896398a5f8ef727aa571e9031a605338a1bc46
Agustín Contreras <aguscont@Agustins-MacBook-Air.local>
2024-02-03 11:00:49 -0700

    AddKittyGifs

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-document.getElementById('yesButton').addEventListener('click', function () {
+const yesGifs = [
+    "hug-kiss.gif",
+    "kissy-cats.gif",
+    "snow-cats.gif"
+    // Add more for Yes Kitty
+];
+
+const noGifs = [
+    "angry-cats.gif",
+    "annoy-cat.gif",
+    "huh-cat.gif"
+    // Add more for No Kitty
+];
+
+document.getElementById('yesButton').addEventListener('click', function() {
     displayGif(true);
 });
 
-document.getElementById('noButton').addEventListener('click', function () {
+document.getElementById('noButton').addEventListener('click', function() {
     displayGif(false);
 });
 
 function displayGif(answer) {
     const gifContainer = document.getElementById('gifContainer');
+    let selectedGif;
     if (answer) {
-        gifContainer.innerHTML = '<img src="hug-kiss.gif" alt="Celebration GIF">';
+        selectedGif = yesGifs[Math.floor(Math.random() * yesGifs.length)];
     } else {
-        gifContainer.innerHTML = '<img src="huh-cat.gif" alt="Denial GIF">';
+        selectedGif = noGifs[Math.floor(Math.random() * noGifs.length)];
     }
+    gifContainer.innerHTML = `<img src="${selectedGif}" alt="Response GIF">`;
 }
