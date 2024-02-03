976c4b34622a1540c415f567e3ff49986465b6ec
Agustín Contreras <aguscont@Agustins-MacBook-Air.local>
2024-02-02 23:21:22 -0700

    initialcommit

---
+++ script.js
@@ -0,0 +1,8 @@
+function answer(response) {
+    if(response) {
+        alert("Shiii, I love you cutie ");
+    }
+    else{
+        ("Que Awite, la neta voy llorar amor, pero yo voy seguir amandote");
+    }
+}
